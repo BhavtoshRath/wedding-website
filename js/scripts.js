47e599716d78f96d5ffd30550ec233e5387eb189
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2022-04-28 20:23:14 -0500

    changed invite code MD5

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '57de3f4d81d11c28d268b1d8ea88c14e') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbxwlXwpVudcyUMGloUfQ9cNblV3yvChfb4ZMKFANgPXwlTN9J0Ys93gILu_lQZfr4r0/exec', data)
